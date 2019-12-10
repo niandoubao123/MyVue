fa4c0fae987050c6918d44add6116930ee369cb9
niandoubao123 <2462602081@qq.com>
2019-12-10 15:16:08 +0800

    init my app

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container">
+    <!-- 顶部 -->
+    <mt-header fixed title="郑玉鹏Vue商城"></mt-header>
+    <!-- router-view -->
+
+    <!-- 底部 -->
+     <nav class="mui-bar mui-bar-tab">
+			<router-link class="mui-tab-item" to="/home">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+					<span class="mui-badge">0</span>
+				</span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
+		</nav>
+  </div>
+</template>
+
+<script>
+// 按需导入 Toast 组件
+// import { Toast } from "mint-ui";
+
+export default {
+  
+};
+</script>
+
+<style lang="stylus" scoped>
+  .app-container{
+    padding-top 40px
+  }
+</style>
