3befab4515273f80723b4fab4a1097febcd4bca8
yupengzheng <5537284+yupengzheng@user.noreply.gitee.com>
2019-12-12 18:49:49 +0800

    aaa

--- src/components/goods/GoodsList.vue
+++ src/components/goods/GoodsList.vue
@@ -1,53 +1,88 @@
 <template>
-    <div class="goods-list">
-        <div class="goods-item">
-            <img src="https://qnm.hunliji.com/o_1dluf15lp8emfq118lg9gf1ciq9.png" width="100px" alt="">
-            <h1 class="title">浪漫婚礼</h1>
-            <div class="info">
-                <p class="price">
-                    <span class="now">1199</span>
-                    <span class="old">1799</span>
-                </p>
-                <p class="sell">
-                    <span>热卖中</span>
-                    <span>剩0件</span>
-                </p>
-            </div>
-        </div>
-         <div class="goods-item">
-            <img src="https://qnm.hunliji.com/o_1dluf15lp8emfq118lg9gf1ciq9.png" width="100px" alt="">
-            <h1 class="title">浪漫婚礼</h1>
-            <div class="info">
-                <p class="price">
-                    <span class="now">1199</span>
-                    <span class="old">1799</span>
-                </p>
-                <p class="sell">
-                    <span>热卖中</span>
-                    <span>剩0件</span>
-                </p>
-            </div>
-        </div>
-         <div class="goods-item">
-            <img src="https://qnm.hunliji.com/o_1dluf15lp8emfq118lg9gf1ciq9.png" alt="">
-            <h1 class="title">浪漫婚礼</h1>
-            <div class="info">
-                <p class="price">
-                    <span class="now">1199</span>
-                    <span class="old">1799</span>
-                </p>
-                <p class="sell">
-                    <span>热卖中</span>
-                    <span>剩0件</span>
-                </p>
-            </div>
-        </div>
+     <div class="goods-list">
+    
+    <!-- <router-link class="goods-item" v-for="item in goodslist" :key="item.id" :to="'/home/goodsinfo/' + item.id" tag="div">
+      <img :src="item.img_url" alt="">
+      <h1 class="title">{{ item.title }}</h1>
+      <div class="info">
+        <p class="price">
+          <span class="now">￥{{ item.sell_price }}</span>
+          <span class="old">￥{{ item.market_price }}</span>
+        </p>
+        <p class="sell">
+          <span>热卖中</span>
+          <span>剩{{ item.stock_quantity }}件</span>
+        </p>
+      </div>
+    </router-link> -->
+
+    <!-- 在网页中，有两种跳转方式： -->
+    <!-- 方式1： 使用 a 标签 的形式叫做 标签跳转  -->
+    <!-- 方式2： 使用 window.location.href 的形式，叫做 编程式导航 -->
+    <div class="goods-item" v-for="item in goodslist" :key="item.id" @click="goDetail(item.id)">
+      <img :src="item.img_url" alt="">
+      <h1 class="title">{{ item.title }}</h1>
+      <div class="info">
+        <p class="price">
+          <span class="now">￥{{ item.sell_price }}</span>
+          <span class="old">￥{{ item.market_price }}</span>
+        </p>
+        <p class="sell">
+          <span>热卖中</span>
+          <span>剩{{ item.stock_quantity }}件</span>
+        </p>
+      </div>
     </div>
+
+
+    <mt-button type="danger" size="large" @click="getMore">加载更多</mt-button>
+  </div>
 </template>
 
 <script>
 export default {
-    
+      data() {
+    // data 是往自己组件内部，挂载一些私有数据的
+    return {
+      pageindex: 1, // 分页的页数
+      goodslist: [] // 存放商品列表的数组
+    };
+  },
+  created() {
+    this.getGoodsList();
+  },
+  methods: {
+    getGoodsList() {
+      // 获取商品列表
+      this.$http
+        .get("api/getgoods?pageindex=" + this.pageindex)
+        .then(result => {
+          if (result.body.status === 0) {
+            // this.goodslist = result.body.message;
+            this.goodslist = this.goodslist.concat(result.body.message);
+          }
+        });
+    },
+    getMore() {
+      this.pageindex++;
+      this.getGoodsList();
+    },
+    goDetail(id) {
+      // 使用JS的形式进行路由导航
+
+      // 注意： 一定要区分 this.$route 和 this.$router 这两个对象，
+      // 其中： this.$route 是路由【参数对象】，所有路由中的参数， params, query 都属于它
+      // 其中： this.$router 是一个路由【导航对象】，用它 可以方便的 使用 JS 代码，实现路由的 前进、后退、 跳转到新的 URL 地址
+
+      console.log(this);
+      // 1. 最简单的
+      this.$router.push("/home/goodsinfo/" + id);
+      // 2. 传递对象
+    //   this.$router.push({ path: "/home/goodsinfo/" + id });
+      // 3. 传递命名的路由  这里要把路由添加name:"goodsinfo"
+    //   this.$router.push({ name: "goodsinfo", params: { id } });
+    }
+  }
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -10,6 +10,9 @@ import NewsInfo from './components/news/NewsInfo.vue'
 import PhotoList from './components/photos/PhotoList.vue'
 import PhotoInfo from './components/photos/PhotoInfo.vue'
 import GoodsList from './components/goods/GoodsList.vue'
+import GoodsInfo from './components/goods/GoodsInfo.vue'
+import GoodsDesc from './components/goods/GoodsDesc.vue'
+import GoodsComment from './components/goods/GoodsComment.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -24,6 +27,9 @@ var router = new VueRouter({
    { path: '/home/photolist',component:PhotoList},
    { path: '/home/photoinfo/:id',component:PhotoInfo},
    { path: '/home/goodslist',component:GoodsList},
+   { path: '/home/goodsinfo/:id',component:GoodsInfo},
+   { path: '/home/goodsdesc/:id',component:GoodsDesc},
+   { path: '/home/goodscomment/:id',component:GoodsComment},
   ],
   linkActiveClass:'mui-active'//tab点击高亮   替换默认的路由的类router-link-active
 })
